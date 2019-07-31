9a3fa9e2cf30b93d010dd50265de89be8f2f4006
Nian-Hu <nianhu96@gmail.com>
2019-07-31 09:13:07 -0400

    Seed database with users and listings, accounting for associations between the two

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,38 +47,48 @@ ashlyn = User.create!(email: 'ashlyndrake@gmail.com',
                     lname: 'Drake', 
                     password: 'hunter12')
 
+# demo.listings.create!(host_id: 1,
+#                       title: "My sweet pad",
+#                       description: "The most beautiful house",
+#                       address: "12 Sunshine St.",
+#                       city: "Venice, Italy",
+#                       lat: 50.698959,
+#                       long: 50.084640,
+#                       price: 50)
+
+
 ################
 ### LISTINGS ###
 ################
 
-Listing.destroy_all
+# Listing.destroy_all
 
-iceland = Listing.create!(host_id: 2,
-                          title: "Whimsical cottage in Iceland",
-                          description: "For the intrepid traveler who craves solitude and space for contemplation, this remote cottage located seemingly in the middle of nowhere of Iceland may be your calling. Based on the grounds of a family-run horse farm, this whimsical little hidey-hole is perfect for someone who needs to escape civilization for one, two, maybe seven days. Dive deep into the bleakness of the black sand beaches, feel the wild winds whip around your face as you stand atop a mountain, and gaze awe-struck at the sheer power of unbridled nature. If vast, wind-blown expanses and fog-filled landscapes strike excitement rather than fear in your heart, then look no further — this house is for you.",
-                          address: "Hofdabrekku 871",
-                          city: "Vík, Iceland",
-                          lat: 63.514858,
-                          long: 18.734949,
-                          price: 50)
+# iceland = Listing.create!(host_id: 2,
+#                           title: "Whimsical cottage in Iceland",
+#                           description: "For the intrepid traveler who craves solitude and space for contemplation, this remote cottage located seemingly in the middle of nowhere of Iceland may be your calling. Based on the grounds of a family-run horse farm, this whimsical little hidey-hole is perfect for someone who needs to escape civilization for one, two, maybe seven days. Dive deep into the bleakness of the black sand beaches, feel the wild winds whip around your face as you stand atop a mountain, and gaze awe-struck at the sheer power of unbridled nature. If vast, wind-blown expanses and fog-filled landscapes strike excitement rather than fear in your heart, then look no further — this house is for you.",
+#                           address: "Hofdabrekku 871",
+#                           city: "Vík, Iceland",
+#                           lat: 63.514858,
+#                           long: 18.734949,
+#                           price: 50)
 
-brooklyn = Listing.create!(host_id: 2,
-                          title: "Airy loft in Williamsburg",
-                          description: "For the urban explorer seeking escape into the hidden corners of the city, look no further than this light-filled loft located in the heart of Williamsburg. Just a few steps away from countless artisanal markets, vintage shopping, and farm-to-table restaurants, this place is ideal for the solo traveler who disdains the swarming crowds and bright lights of Times Square and craves a grittier, more authentic representation of the world’s most famous city. Get lost, strike up a conversation with the barista down the street, or make new friends at the nearby dive bar — this city is yours to explore.",
-                          address: "84 Withers St",
-                          city: "New York, USA",
-                          lat: 40.716880,
-                          long: -73.948810,
-                          price: 60)
+# brooklyn = Listing.create!(host_id: 2,
+#                           title: "Airy loft in Williamsburg",
+#                           description: "For the urban explorer seeking escape into the hidden corners of the city, look no further than this light-filled loft located in the heart of Williamsburg. Just a few steps away from countless artisanal markets, vintage shopping, and farm-to-table restaurants, this place is ideal for the solo traveler who disdains the swarming crowds and bright lights of Times Square and craves a grittier, more authentic representation of the world’s most famous city. Get lost, strike up a conversation with the barista down the street, or make new friends at the nearby dive bar — this city is yours to explore.",
+#                           address: "84 Withers St",
+#                           city: "New York, USA",
+#                           lat: 40.716880,
+#                           long: -73.948810,
+#                           price: 60)
 
-bali = Listing.create!(host_id: 2,
-                      title: "Luxurious villa in Bali",
-                      description: "For the overworked, perpetually sleep-deprived jetsetter who craves nothing more than a deep-tissue massage by the jungle and an early-morning dip in a private pool, this luxurious villa located in the heart of Bali is exactly what the doctor ordered. Surely you did not think that luxury villas were solely the domain of honeymooning couples or raucous groups of friends? Whoever said that you can’t take a five-hour-long flower bath by yourself was certainly misguided. At this luxury villa, you can receive the pampering that you need to make your body less sore as well as the glamorous Instagram photos that you need to make your friends back home more jealous.",
-                      address: "Jl. Raya Nyuh Kuning Pengosekan",
-                      city: "Ubud, Indonesia",
-                      lat: -8.533019,
-                      long: 115.266617, 
-                      price: 40)
+# bali = Listing.create!(host_id: 2,
+#                       title: "Luxurious villa in Bali",
+#                       description: "For the overworked, perpetually sleep-deprived jetsetter who craves nothing more than a deep-tissue massage by the jungle and an early-morning dip in a private pool, this luxurious villa located in the heart of Bali is exactly what the doctor ordered. Surely you did not think that luxury villas were solely the domain of honeymooning couples or raucous groups of friends? Whoever said that you can’t take a five-hour-long flower bath by yourself was certainly misguided. At this luxury villa, you can receive the pampering that you need to make your body less sore as well as the glamorous Instagram photos that you need to make your friends back home more jealous.",
+#                       address: "Jl. Raya Nyuh Kuning Pengosekan",
+#                       city: "Ubud, Indonesia",
+#                       lat: -8.533019,
+#                       long: 115.266617, 
+#                       price: 40)
 
   
                           
