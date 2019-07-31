d82190a25e1e9563daf2ceed5a4f2d23e8231cf9
Nian-Hu <nianhu96@gmail.com>
2019-07-31 11:02:55 -0400

    Create marker manager for map

---
+++ frontend/util/marker_manager.js
@@ -0,0 +1,40 @@
+export default class MarkerManager {
+  constructor(map) {
+    this.map = map;
+    this.markers = {};
+  }
+
+  updateMarkers(listings) {
+    listings.forEach(listing => {
+      if (!Object.keys(this.markers).includes(listing.id)) {
+        this.createMarkerFromListing(listing);
+      }
+    })
+
+    const listingObj = {};
+    listings.forEach(listing => {
+      listingObj[listing.id] = listing;
+    })
+
+    Object.values(this.markers).forEach(marker => {
+      if (!Object.keys(listingObj).includes(marker.listingId)) {
+        this.removeMarker(this.markers[listingId])
+      }
+    })
+  }
+
+  createMarkerFromListing(listing) {
+    const position = new google.maps.LatLng(listing.lat, listing.lng)
+    const marker = new google.maps.Marker({
+      position,
+      map: this.map,
+      listingId: listing.id
+    })
+    this.markers[listing.id] = marker;
+  }
+
+  removeMarker(marker) {
+    this.markers[marker.listingId].setMap(null);
+    delete this.markers[marker.listingId];
+  }
+}
