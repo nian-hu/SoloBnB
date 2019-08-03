f2d37f3ecd8b69b86265043f37dccc87972a22af
Nian-Hu <nianhu96@gmail.com>
2019-08-03 18:15:25 -0400

    Create bookings reducer and add it to entities reducer

---
+++ frontend/reducers/bookings_reducer.js
@@ -0,0 +1,26 @@
+import { RECEIVE_ALL_BOOKINGS, RECEIVE_BOOKING, REMOVE_BOOKING } from '../actions/booking_actions';
+import { merge } from 'lodash';
+
+const bookingsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_ALL_BOOKINGS: {
+      return merge({}, action.bookings);
+    } 
+    case RECEIVE_BOOKING: {
+      const newState = merge({}, state);
+      newState[action.booking.id] = action.booking;
+      return newState;
+    }
+    case REMOVE_BOOKING: {
+      const newState = merge({}, state);
+      delete newState[action.bookingId];
+      return newState;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export default bookingsReducer;
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import listingsReducer from './listings_reducer';
 import amenitiesReducer from './amenities_reducer';
+import bookingsReducer from './bookings_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   listings: listingsReducer,
-  amenities: amenitiesReducer
+  amenities: amenitiesReducer,
+  bookings: bookingsReducer
 });
 
 export default entitiesReducer;
