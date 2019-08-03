7b74ccb1db5f712e437bde8c6de708a94955045a
Nian-Hu <nianhu96@gmail.com>
2019-08-03 12:23:49 -0400

    Style the amenities modal

--- frontend/components/listing_show/amenities_modal.jsx
+++ frontend/components/listing_show/amenities_modal.jsx
@@ -7,16 +7,20 @@ class AmenitiesModal extends React.Component {
 
     const amenityItems = this.props.amenities.map((amenity, idx) => {
       return (
-        <li key={idx}>
+        <li className='amenity-modal-item' key={idx}>
           {amenity.name}
         </li>
       )
     })
 
     return (
-      <ul>
-        {amenityItems}
-      </ul>
+      <div>
+        <div className='close-button topleft' onClick={this.props.closeModal}>&times;</div>
+        <h1 className='amenity-modal-title'>Amenities</h1>
+        <ul className='amenity-modal-list'>
+          {amenityItems}
+        </ul>
+      </div>
     )
     
   }
