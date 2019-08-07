acce80cdef9189a8afd637c9309496cb266253ad
Nian-Hu <nianhu96@gmail.com>
2019-08-06 23:52:25 -0400

    Display rating stars organized in two columns above the reviews

--- frontend/components/reviews/review_index.jsx
+++ frontend/components/reviews/review_index.jsx
@@ -9,16 +9,43 @@ class ReviewIndex extends React.Component {
     // this.props.fetchReviews()
   }
 
+  renderStars() {
+    const stars = [];
+
+    for (let i = 0; i < 5; i++) {
+      stars.push(<i key={i} className="far fa-star"></i>)
+    }
+
+    return stars;
+  }
+
   render() {
     const { reviews } = this.props;
     if (!reviews) {
       return <div className="loader">Loading...</div>
     }
 
+    const stars = <div className='stars-row'>{this.renderStars()}</div>
+    
     if (reviews.length === 0) {
       return (
-        <div className='review-content'>
-          No reviews yet.
+        <div>
+          <div className='review-stars'>
+            <div className='stars-col1'>
+              {stars}
+              {stars}
+              {stars}
+            </div>
+            <div className='stars-col2'>
+              {stars}
+              {stars}
+              {stars}
+            </div>
+          </div>
+
+          <div className='review-content'>
+            No reviews yet.
+          </div>
         </div>
       )
     }
