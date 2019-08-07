14c8b9f097ac4578de8c46618a69edc80b2bc3a8
Nian-Hu <nianhu96@gmail.com>
2019-08-07 09:56:07 -0400

    Calculate average rating of each category and render stars accordingly

--- frontend/components/reviews/review_index.jsx
+++ frontend/components/reviews/review_index.jsx
@@ -9,15 +9,61 @@ class ReviewIndex extends React.Component {
     // this.props.fetchReviews()
   }
 
-  renderStars() {
+  renderStars(num) {
     const stars = [];
 
     for (let i = 0; i < 5; i++) {
-      stars.push(<i key={i} className="far fa-star"></i>)
+      if (i < num) {
+        stars.push(<i key={i} className="fas fa-star"></i>)
+      } else {
+        stars.push(<i key={i} className="far fa-star"></i>)
+      }
     }
 
     return stars;
   }
+
+  
+  // let reviews = [
+  //   {id: 1, accuracy: 5, communication: 5, cleanliness: 5, location: 5, check_in: 5, value: 5},
+  //   {id: 2, accuracy: 1, communication: 1, cleanliness: 1, location: 1, check_in: 1, value: 1}
+  // ]
+
+  findAvgRating() {
+    const { reviews } = this.props;
+    const ratings = { 
+      accuracy: 0, 
+      communication: 0,
+      cleanliness: 0,
+      location: 0,
+      check_in: 0,
+      value: 0
+    }
+
+    // let accuracy = 0;
+    for (let i = 0; i < reviews.length; i++) {
+      let review = reviews[i];
+      
+      for (let key in ratings) {
+        ratings[key] += review[key]
+      }
+    }
+    // { accuracy: 24, communication: 24, etc }
+
+    for (let key in ratings) {
+      ratings[key] = (ratings[key] / reviews.length)
+    }
+
+    return ratings;
+  }
+
+  // ratings = 
+  // { accuracy: 3,
+  // communication: 3,
+  // cleanliness: 3,
+  // location: 3,
+  // check_in: 3,
+  // value: 3 }
 
   render() {
     const { reviews } = this.props;
@@ -25,22 +71,41 @@ class ReviewIndex extends React.Component {
       return <div className="loader">Loading...</div>
     }
 
-    const stars = <div className='stars-row'>{this.renderStars()}</div>
-    
+    const ratingsObj = this.findAvgRating();
+    const acc = ratingsObj.accuracy;
+    const comm = ratingsObj.communication;
+    const clean = ratingsObj.cleanliness;
+    const loc = ratingsObj.location;
+    const checkin = ratingsObj.check_in;
+    const value = ratingsObj.value;
+
+    const accStars = <div className='stars-row'>{this.renderStars(acc)}</div>
+    const commStars = <div className='stars-row'>{this.renderStars(comm)}</div>
+    const cleanStars = <div className='stars-row'>{this.renderStars(clean)}</div>
+    const locStars = <div className='stars-row'>{this.renderStars(loc)}</div>
+    const checkinStars = <div className='stars-row'>{this.renderStars(checkin)}</div>
+    const valueStars = <div className='stars-row'>{this.renderStars(value)}</div>
+
+    const reviewStarSection = (
+      <div className='review-stars'>
+        <div className='stars-col1'>
+          Accuracy {accStars}
+          Communication {commStars}
+          Cleanliness {cleanStars}
+        </div>
+        <div className='stars-col2'>
+          Location {locStars}
+          Check-in {checkinStars}
+          Value {valueStars}
+        </div>
+      </div>
+    )
+
     if (reviews.length === 0) {
       return (
         <div>
-          <div className='review-stars'>
-            <div className='stars-col1'>
-              {stars}
-              {stars}
-              {stars}
-            </div>
-            <div className='stars-col2'>
-              {stars}
-              {stars}
-              {stars}
-            </div>
+          <div>
+            {reviewStarSection}
           </div>
 
           <div className='review-content'>
@@ -60,6 +125,10 @@ class ReviewIndex extends React.Component {
 
     return (
       <div>
+        <div>
+          {reviewStarSection}
+        </div>
+
         <ul className='reviews-list'>
           {reviewItems}
         </ul>
