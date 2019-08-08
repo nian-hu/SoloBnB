06ecaa22609f634d7abac2dcbd585c05f532506d
Nian-Hu <nianhu96@gmail.com>
2019-08-08 11:32:09 -0400

    Create chatroom and message form components

---
+++ frontend/components/messages/message_form.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+class MessageForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      body: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(field) {
+    return (e) => {
+      this.setState({ [field]: e.target.value })
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    App.cable.subscriptions.subscriptions[0].speak({
+      message: this.state.body
+    })
+    this.setState({ body: '' })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            value={this.state.body}
+            onChange={this.handleChange}
+            placeholder='Write your message here'
+          />
+          <input type='submit' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default MessageForm;
