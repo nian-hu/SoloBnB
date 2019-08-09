854d5c54e2ba7a1105e806bedf0872c9ccc053c6
Nian-Hu <nianhu96@gmail.com>
2019-08-09 11:17:59 -0400

    it worked

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_09_142823) do
+ActiveRecord::Schema.define(version: 2019_08_09_141859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,10 +95,10 @@ ActiveRecord::Schema.define(version: 2019_08_09_142823) do
 
   create_table "messages", force: :cascade do |t|
     t.string "body", null: false
+    t.integer "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "channel_id"
-    t.integer "author_id"
   end
 
   create_table "reviews", force: :cascade do |t|
