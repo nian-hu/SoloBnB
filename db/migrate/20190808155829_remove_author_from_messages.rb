c778969b3cbd31779c5bd51b91e113d7cf47908a
Nian-Hu <nianhu96@gmail.com>
2019-08-09 11:03:25 -0400

    try to fix it

--- db/migrate/20190808155829_remove_author_from_messages.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAuthorFromMessages < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :messages, :author_id
-  end
-end
--- db/migrate/20190809142731_remove_author_id_from_messages.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAuthorIdFromMessages < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :messages, :author_id
-  end
-end
